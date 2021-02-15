00068e11262b18d44f748152dede59e385af637e
Nick Chinsen <nicholaschinsen@gmail.com>
2021-02-14 22:55:57 -0500

    Express server

--- index.tsx
+++ index.tsx
@@ -1,18 +1,13 @@
 //load http module
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const app = express();
+const port = 8000;
 
-//create http server listen on port 3000
-const server = http.createServer((req, res) => {
-  //set http response header with http status and content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+//home route
+app.get('/', (req, res) => {
+  res.send('Hellow World!')
 });
 
-//listen for request on port 3000, and as a callback function have 
-//port listened on log
-server.listen(port, hostname, () => {
-  console.log(`Server running at https://${hostname}:${port}/`);
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
