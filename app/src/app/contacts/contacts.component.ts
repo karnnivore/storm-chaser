aea10ccd579981e4280c24bf54f7a1b89ecf9569
Nick Chinsen <nicholaschinsen@gmail.com>
2021-04-09 12:01:01 -0400

    update map and leads component

--- app/src/app/contacts/contacts.component.ts
+++ app/src/app/contacts/contacts.component.ts
@@ -11,5 +11,5 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  typesOfCustomers = [['647-402-5543', 'John Smith', '356 Markham St.'], ['647-241-5533', 'Alex Gogh', '76 George St.'], ['416-432-2423', 'Wynne Law', '42 Carlton St.'], ['647-978-6876', 'Jasper Reynolds', '4221 Yonge St.'], ['647-421-3323', 'Ivory Osborne', '4000 Kennedy Rd.']];
+  typesOfCustomers = [['585-402-5543', 'Rochester, NY 14607, USA', '14 Anson Pl'], ['585-241-5533', 'Rochester, NY 14604, USA', '328 E Main St.'], ['585-432-2423', 'Rochester, NY 14620, USA', '390 South Ave.'], ['585-978-6876', 'Rochester, NY 14605, USA', '114 University Ave.'], ['585-421-3323', 'Rochester, NY 14605, USA', '24 Upper Falls']];
 }
--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -20,8 +20,10 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
+        // lat: position.coords.latitude + 1.458,
+        // lng: position.coords.longitude + 1
+        lat: 43.15636380842052,
+        lng: -77.60472774000483
       }
       this.circleCenter = {
         lat: position.coords.latitude,
